d2c96824d5313f3a931935e0c0ebe886aeee1378
Knut <knut@k-nut.eu>
2013-01-08 20:05:05 +0100

    A little bit of polishments

--- routes.py
+++ routes.py
@@ -16,41 +16,47 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def main():
+    ''' The main page '''
     Stops = DB_Stop.query.all()
     return render_template("index.html", stops=Stops)
 
 @app.route("/stops/<show_only>/<north>/<east>/<south>/<west>")
 def stops_in_bounding_box(show_only, north, east, south, west):
-    if show_only=="problemsonly":
+    ''' Only show stops within a given bounding box. Allow filtering by
+    matches/nomatches'''
+    if show_only == "problemsonly":
         result = DB_Stop.query.filter(
-                DB_Stop.lat.between(float(south),float(north)),
-                DB_Stop.lon.between(float(west), float(east)),
-                DB_Stop.matches==0
-                ).all()
-    elif show_only=="matchesonly":
+            DB_Stop.lat.between(float(south),float(north)),
+            DB_Stop.lon.between(float(west), float(east)),
+            DB_Stop.matches==0
+            ).all()
+
+    elif show_only == "matchesonly":
         result = DB_Stop.query.filter(
-                DB_Stop.lat.between(float(south),float(north)),
-                DB_Stop.lon.between(float(west), float(east)),
-                DB_Stop.matches>0
-                ).all()
+            DB_Stop.lat.between(float(south),float(north)),
+            DB_Stop.lon.between(float(west), float(east)),
+            DB_Stop.matches>0
+            ).all()
     else:
         result = DB_Stop.query.filter(
-                DB_Stop.lat.between(float(south),float(north)),
-                DB_Stop.lon.between(float(west), float(east)),
-                ).all()
+            DB_Stop.lat.between(float(south),float(north)),
+            DB_Stop.lon.between(float(west), float(east)),
+            ).all()
 
-        return render_template("index.html", stops=result)
+    return render_template("index.html", stops = result)
 
 @app.route("/recheck/<id>")
 def recheck(id):
+    ''' Rerun the checks for a stop and update the db'''
     stop = DB_Stop.query.filter_by(id=id).first()
     stop.matches = VBB_Stop(stop.to_vbb_syntax()).is_in_osm()
-    stop.last_run = datetime.datetime.now().replace(microsecond=0);
+    stop.last_run = datetime.datetime.now().replace(microsecond=0)
     db.session.commit()
     return redirect(url_for("main"))
 
 @app.route("/nomatch_all")
 def nomatch_all():
+    ''' Return a json file of all stops that are not in OSM '''
     Stops = DB_Stop.query.filter(DB_Stop.matches < 1).all()
     all_stops = []
     for Stop in Stops:
@@ -59,10 +65,13 @@ def nomatch_all():
 
 @app.route("/map_of_the_bad")
 def map_of_the_bad():
+    ''' Return a map with all the stops that aren't in OSM '''
     return render_template("map.html")
 
 def get_trains():
-    req = requests.get("http://datenfragen.de/openvbb/GTFS_VBB_Okt2012/routes.txt")
+    ''' The initial query to set up the train db '''
+    url = "http://datenfragen.de/openvbb/GTFS_VBB_Okt2012/routes.txt"
+    req = requests.get(url)
     text = req.text.split("\n")
     all_bvg_lines = []
     for line in text:
@@ -73,7 +82,9 @@ def get_trains():
     return all_bvg_lines
 
 def get_stops():
-    req = requests.get("http://datenfragen.de/openvbb/GTFS_VBB_Okt2012/stops.txt")
+    ''' The initial query to set up the stop db '''
+    url = "http://datenfragen.de/openvbb/GTFS_VBB_Okt2012/stops.txt"
+    req = requests.get(url)
     text = req.text.split("\n")[1:]
     for line in text:
         if len(line) > 1:
@@ -95,6 +106,7 @@ def get_stops():
                     db.session.commit()
 
 class DB_Stop(db.Model):
+    ''' The represenation of a stop in the database '''
     id = db.Column(db.Integer, primary_key=True)
     last_run = db.Column(db.DateTime)
     name = db.Column(db.String(200), index=True)
@@ -111,13 +123,15 @@ class DB_Stop(db.Model):
         self.last_run = datetime.datetime.now().replace(microsecond=0)
 
     def to_vbb_syntax(self):
-        return '%i,,"%s",,%f,%f'%(self.id, self.name, self.lat, self.lon)
+        ''' Return a line that looks as if it comes from the routes.txt '''
+        return '%i,,"%s",,%f,%f' % (self.id, self.name, self.lat, self.lon)
 
     def __repr__(self):
         return '<Stop %r>' % self.name
 
 
 class VBB_Stop():
+    ''' Takes a line from the stops.txt and returns a VBB_Stop object '''
     def __init__(self, line_from_stops_txt):
         #since csvreader doesnt understand unicode we'll just use good old regex
         pattern = re.compile('(\d*),,"(.*)",,(\d*\.\d*),(\d*\.\d*)')
@@ -127,24 +141,29 @@ class VBB_Stop():
         self.lat = float(fields[2])
         self.lon = float(fields[3])
 
-        def is_in_osm(self):
-            north = self.lat - 0.002
-            east =  self.lon - 0.002
-            south = self.lat + 0.002
-            west =  self.lon + 0.002
-            short_name = self.name
-            if "(" in self.name: #remove the (Berlin) from the line
-                short_name = short_name.split(" (")[0]
-            if "/" in self.name:
-                short_name = short_name.split("/")[1] # if we have something like StreetA/StreetB overpass is not able to find this
-                                                      # we can just look for StreetB though since we are limiting the search on a small ara
-                payload = {"data":'[output:json];node(%f, %f, %f, %f)["name"~"%s"];out skel;'%(north, east, south, west, short_name)}
-                r = requests.get("http://overpass-api.de/api/interpreter", params=payload)
-                this_json = json.loads(r.text)
-                stations = this_json.get("elements")
-                return len(stations)
+    def is_in_osm(self):
+        ''' Call Overpass to see if there is an object with the given name
+            close to the cordinates given '''
+        north = self.lat - 0.002
+        east =  self.lon - 0.002
+        south = self.lat + 0.002
+        west =  self.lon + 0.002
+        short_name = self.name
+        if "(" in self.name: #remove the (Berlin) from the line
+            short_name = short_name.split(" (")[0]
+
+        # if we have something like StreetA/StreetB overpass is not able to find this
+        # we can just look for StreetB though since we are limiting the search on a small ara
+        if "/" in self.name:
+            short_name = short_name.split("/")[1]
+        payload = {"data":'[output:json];node(%f, %f, %f, %f)["name"~"%s"];out skel;'%(north, east, south, west, short_name)}
+        r = requests.get("http://overpass-api.de/api/interpreter", params=payload)
+        this_json = json.loads(r.text)
+        stations = this_json.get("elements")
+        return len(stations)
 
 class Bvg_line():
+    ''' Takes a line from routes.txt and return a Bvg_line object '''
     def __init__(self, line_from_routes_txt):
         fields = line_from_routes_txt.split(",")
         self.bvg_id = fields[0]
@@ -170,7 +189,7 @@ class Bvg_line():
             self.transit_type = self.agency
 
         def is_in_osm():
-            payload = {"data":'[output:json];relation["network"="VBB"]["ref"="%s"];out;'%self.line_number}
+            payload = {"data":'[output:json];relation["network"="VBB"]["ref"="%s"];out;'% self.line_number}
             r = requests.get("http://overpass-api.de/api/interpreter", params=payload)
             if "tags" in r.text:
                 return r.text.split('"name": "')[1].split('"')[0]
@@ -178,11 +197,13 @@ class Bvg_line():
                 return False
 
 def print_success(message):
-    print '\033[1;32m%s\033[1;m'%(message)
+    ''' Print the message in green '''
+    print '\033[1;32m%s\033[1;m'% (message)
 
 def print_failure(message):
-    print '\033[1;31m%s\033[1;m'%(message)
+    ''' Print the message in red '''
+    print '\033[1;31m%s\033[1;m'% (message)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.debug = True
     app.run()

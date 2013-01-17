1605812c69d2c34b7f0c92e2071adeed0336627c
Knut <knut@k-nut.eu>
2013-01-17 18:24:56 +0100

    Pep8 changes

--- routes.py
+++ routes.py
@@ -1,15 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import requests
-from flask import Flask, redirect, url_for, \
-        render_template, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, url_for, \
+    render_template, jsonify, request
 import datetime
-import os
-
-import config
 
 from models import DB_Stop, DB_Train, VBB_Stop, Bvg_line, app, db
+
 
 @app.route("/")
 def main():
@@ -17,30 +14,32 @@ def main():
     Stops = DB_Stop.query.all()
     return render_template("index.html", stops=Stops)
 
+
 @app.route("/stops/<show_only>/<north>/<east>/<south>/<west>")
 def stops_in_bounding_box(show_only, north, east, south, west):
     ''' Only show stops within a given bounding box. Allow filtering by
     matches/nomatches'''
     if show_only == "problemsonly":
         result = DB_Stop.query.filter(
-            DB_Stop.lat.between(float(south),float(north)),
+            DB_Stop.lat.between(float(south), float(north)),
             DB_Stop.lon.between(float(west), float(east)),
-            DB_Stop.matches==0
-            ).all()
+            DB_Stop.matches == 0
+        ).all()
 
     elif show_only == "matchesonly":
         result = DB_Stop.query.filter(
-            DB_Stop.lat.between(float(south),float(north)),
+            DB_Stop.lat.between(float(south), float(north)),
             DB_Stop.lon.between(float(west), float(east)),
-            DB_Stop.matches>0
-            ).all()
+            DB_Stop.matches > 0
+        ).all()
     else:
         result = DB_Stop.query.filter(
-            DB_Stop.lat.between(float(south),float(north)),
+            DB_Stop.lat.between(float(south), float(north)),
             DB_Stop.lon.between(float(west), float(east)),
-            ).all()
+        ).all()
 
-    return render_template("index.html", stops = result)
+    return render_template("index.html", stops=result)
+
 
 @app.route("/recheck/<id>")
 def recheck(id, from_cm_line=False):
@@ -54,15 +53,17 @@ def recheck(id, from_cm_line=False):
     else:
         if stop.matches > 0:
             print_success("%s now matches %i stops" % (stop.name,
-                stop.matches));
+                    stop.matches))
         else:
             print_failure("%s does not match any stops..." % (stop.name))
         return True
+
 
 @app.route("/map_of_the_bad")
 def map_of_the_bad():
     ''' Return a map with all the stops that aren't in OSM '''
     return render_template("map.html")
+
 
 @app.route("/api/stops")
 def api_stops():
@@ -79,12 +80,12 @@ def api_stops():
         all_stops.append(stop.to_dict())
     return jsonify(stops=all_stops)
 
+
 def get_trains():
     ''' The initial query to set up the train db '''
     url = "http://datenfragen.de/openvbb/GTFS_VBB_Okt2012/routes.txt"
     req = requests.get(url)
     text = req.text.split("\n")
-    all_bvg_lines = []
     for line in text:
         Train = Bvg_line(line)
         if Train.operator in ["BVG", "DB"]:
@@ -97,10 +98,12 @@ def get_trains():
             db.session.add(new_stop)
             db.session.commit()
 
+
 def recheck_all_missings_stops():
     Stops = DB_Stop.query.filter(DB_Stop.matches < 1).all()
     for Stop in Stops:
         recheck(Stop.id, from_cm_line=True)
+
 
 def get_stops():
     ''' The initial query to set up the stop db '''
@@ -117,23 +120,25 @@ def get_stops():
                 else:
                     print_failure(Stop.name + ":  0")
                     new_stop = DB_Stop(
-                            name = Stop.name,
-                            lat = Stop.lat,
-                            lon = Stop.lon,
-                            matches = feedback,
-                            vbb_id = Stop.stop_id
-                            )
+                        name=Stop.name,
+                        lat=Stop.lat,
+                        lon=Stop.lon,
+                        matches=feedback,
+                        vbb_id=Stop.stop_id
+                    )
                     db.session.add(new_stop)
                     db.session.commit()
 
 
 def print_success(message):
     ''' Print the message in green '''
-    print '\033[1;32m%s\033[1;m'% (message)
+    print '\033[1;32m%s\033[1;m' % (message)
+
 
 def print_failure(message):
     ''' Print the message in red '''
-    print '\033[1;31m%s\033[1;m'% (message)
+    print '\033[1;31m%s\033[1;m' % (message)
+
 
 if __name__ == "__main__":
     app.debug = True

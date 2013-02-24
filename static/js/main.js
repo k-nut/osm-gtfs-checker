30855340ebb542de3daf1ac12ed2e27efccbc26f
Knut <knut@k-nut.eu>
2013-02-24 22:45:28 +0100

    Add in-page view of vbb position

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,32 @@ function initmap() {
 
 	map.addLayer(osm);
 }
+
+function radiusmap(lat, lon) {
+	if (map_exists === false){
+		map_exists = true;
+		radius_map = new L.Map('radius_map').setView([lat, lon], 16);
+
+		// create the tile layer with correct attribution
+		var osmUrl='http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png';
+		var osmAttrib='Map data © OpenStreetMap contributors';
+		var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 18, attribution: osmAttrib});
+		radius_map.addLayer(osm);
+		radius_map.on("moveend", function(){change_link_for_remote_edit();});
+	}
+	var circle = L.circle([lat, lon], 250, {
+		color: 'blue',
+		fillColor: 'lightblue',
+		fillOpacity: 0.75
+	}).addTo(radius_map);
+	var marker = new L.marker([lat, lon]).addTo(radius_map);
+	radius_map.panTo([lat, lon]);
+
+	$("#search_radius").modal();
+	setTimeout(function(){
+		L.Util.requestAnimFrame(radius_map.invalidateSize, radius_map, !1, radius_map._container);
+	}, 500);
+}
 function hiderows(element){
 	if (element.attr("checked")){
 		$("."+element.attr("name")).each(function(){

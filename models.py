1f09dda34556f6b572544bb93815930bc5d8779d
Knut Hühne <knut@k-nut.eu>
2022-08-14 11:58:12 +0200

    Add compatabilty option for heroku postgres & new sqlalchemy

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ import json
 from helpers import get_county
 
 app = Flask(__name__, instance_relative_config=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
